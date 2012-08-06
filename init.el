f5058a05ba25153ada16580daf30f541f50bdedf
Dan Setterquist <dansetterquist@gmail.com>
2012-08-06 14:06:06 +0200

    Bind backspace to C-h.

--- init.el
+++ init.el
@@ -13,3 +13,7 @@
 (add-to-list 'load-path "~/.emacs.d/submodules/expand-region/")
 (require 'expand-region)
 (define-key global-map (kbd "C-ä") 'er/expand-region)
+
+;; Use shell-like backspace C-h, rebind help to F1
+(define-key key-translation-map [?\C-h] [?\C-?])
+(global-set-key (kbd "<f1>") 'help-command)
