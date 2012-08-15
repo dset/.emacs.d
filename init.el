ac0e0af8b40a16984328b9b5de83c122b8c6c355
Dan Setterquist <dansetterquist@gmail.com>
2012-08-15 14:04:10 +0200

    Added magit.

--- init.el
+++ init.el
@@ -24,6 +24,9 @@
 (require 'yasnippet)
 (yas/global-mode 1)
 
+;; Add magit
+(require 'magit)
+
 ;; Use shell-like backspace C-h, rebind help to F1
 (define-key key-translation-map [?\C-h] [?\C-?])
 (global-set-key (kbd "<f1>") 'help-command)
